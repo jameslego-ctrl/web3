df775d78d398f5e75a600e0070604c66fa39f350
James <jameslego222@gmail.com>
2024-11-10 19:35:44 +0530

    description added

--- Lessons/storageFactory/storageFactory.sol
+++ Lessons/storageFactory/storageFactory.sol
@@ -2,6 +2,14 @@
 
 pragma solidity ^0.8.18;
 
+//adding the contract "SimpleStorage" from simpleStorage.sol
+//another way to do is >>>>>>>      import "./simpleStorage.sol";
+// it will add all the file simpleStorage.sol
+
+// if we want to use a specific contract instead of all the file we use the following command
+// import {SimpleStorage,contract2,contract3,.....} from "./simpleStorage.sol"
+//          contract name           :   multiple contract can be imported
+
 
 contract SimpleStorage {
     uint256 myFavoriteNumber;
@@ -32,5 +40,12 @@ contract SimpleStorage {
 
 
 contract StorageFactory{
+    SimpleStorage public simplestorage;
+
+    function createSimpleStorage() public {
+        simplestorage = new SimpleStorage();
+    }
+    // new keyword is used to make new contract 
+    // here simplestorage is a new contract same as SimpleStorage created using the contract StorageFactory
 
 }
