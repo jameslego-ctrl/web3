1fdb788a79daa1450b5a3f402d3b49191c536009
James <jameslego222@gmail.com>
2024-11-12 19:19:21 +0530

    adding FiveAddStorage.sol

--- Lessons/simplestorage/2simplestorage.sol
+++ Lessons/simplestorage/2simplestorage.sol
@@ -2,9 +2,35 @@
 
 pragma solidity ^0.8.19;
 
-contract simplestorage{
 
-    uint256 favoriteNumber;         //gets initialised to 0 if no value is given
-    
+contract SimpleStorage {
+    uint256 myFavoriteNumber;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
 
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
---
+++ Lessons/simplestorage/AddFiveStorage.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+
+import {SimpleStorage} from "./2simplestorage.sol";
+
+// Inheritence : AddFiveStorage is inheriting all the functionality of SimpleStorage 
+//               inheritence allow to use all the functions of an contract in an diff contract
+
+
+contract AddFiveStorage is SimpleStorage{
+
+}
+
