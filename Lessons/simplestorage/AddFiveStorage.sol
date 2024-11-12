d52187e6ea1f2cafc09b1df7a2ad64b54a651798
James <jameslego222@gmail.com>
2024-11-12 19:54:30 +0530

    updated AddFiveStorage

--- Lessons/simplestorage/AddFiveStorage.sol
+++ Lessons/simplestorage/AddFiveStorage.sol
@@ -9,7 +9,21 @@ import {SimpleStorage} from "./2simplestorage.sol";
 //               inheritence allow to use all the functions of an contract in an diff contract
 
 
+
+//here we are incrementing the value of myFavoriteNumber using the store function that is present inside the SimpleStorage contract
+// using the contract AddFiveStorage with the help of inheritance
+
+
 contract AddFiveStorage is SimpleStorage{
+
+    // +5
+    // override : allows the modification of a function from an another contract
+    // virtual override : the parent contract should use the "virtual" func in the parent function 
+    //                     to allow the override of the functionality
+
+    function store(uint256 _newNumber) public override{
+        myFavoriteNumber = _newNumber + 5;
+    }
 
 }
 
