d6d9956680700b29baba5d8832dbb6e47ab1d43a
James <jameslego222@gmail.com>
2025-07-15 21:20:09 +0530

    simple changes & fix some typo

--- Lessons/simplestorage/simplestorage.sol
+++ Lessons/simplestorage/simplestorage.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.19;
 
 contract simpleStorage{
-    //Datatypes : bool,int,uint,string,bytes
+    //Datatypes : boolean,integers,unsigned integers,strings,bytes
+    //Datatypes : bool,     int,        uint,          string,bytes
 
     bool hasFavoriteNumber = false;        // true or false
 
     uint256 FavoriteNumber = 88;          //uint and uint256 means the same we can specify diff bits to uint
 
-    int256 favoriteNumber = -88;          //int and uint 256 means the same we can specify diff bits to int
+    int256 favoriteNumber = -88;          //int and int256 means the same we can specify diff bits to int
 
     string favorite_number = "eighty-eight";
 
