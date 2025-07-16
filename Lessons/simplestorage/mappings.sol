880dcb63b2d463cf8bacc9c3c21e4c0a7676c4b9
James <jameslego222@gmail.com>
2025-07-16 20:55:39 +0530

    info added

--- Lessons/simplestorage/mappings.sol
+++ Lessons/simplestorage/mappings.sol
@@ -3,25 +3,27 @@
 pragma solidity ^0.8.18;
 
 contract simplestorage{
-    //mappings using the mapping()
-    //if there is large num of data in an array 
+
+
+    // MAPPING using the mapping()
+    // if there is large num of data in an array 
     // we can use mapping to read the data using the name or other parameters
 
-    uint256 public favoriteNum;
-
-    struct person{
-        uint256 _favNum;
-        string _name;
-    }
-//  array of person
-    person[] public listofperson;
-
-
-//  we are mapping the name with their fav num while adding the data of the person
-    function addperson(string memory name,uint256 num) public {
-        listofperson.push(person(num,name));
-        TofavNum[name] = num;               // using the mapping
-    }
+  struct person {
     
-    mapping (string => uint256) public TofavNum;
+    uint256 favoriteNumber;
+    string name;
+  }
+
+  person[] public listofpeople;
+
+  mapping(string => uint256) public nametoNumber;
+  mapping(uint256 => string) public numbertoName;
+
+
+  function addperson(string memory _name, uint256 _favoriteNumber) public{
+    listofpeople.push(person(_favoriteNumber,_name));
+    nametoNumber[_name] = _favoriteNumber;
+    numbertoName[_favoriteNumber] = _name;
+  }
 }
