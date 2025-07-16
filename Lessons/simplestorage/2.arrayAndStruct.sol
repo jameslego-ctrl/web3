7a56affc2bec2b91a300c509da133b31d3d8fa86
James <jameslego222@gmail.com>
2025-07-16 13:12:19 +0530

    added more info about pushing into an array

--- Lessons/simplestorage/2.arrayAndStruct.sol
+++ Lessons/simplestorage/2.arrayAndStruct.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.19;
 
 contract simpleStorage{
 
+    //Dynamic Array : the size of the array can change according to the data input in it
     uint256[] listofFavoriteNumber;                     //this is an array of uint256 with no data in it
-    uint256[] public listofFavoriteNumber2 = [1,2,3];   //this is an array with the data uint ,indexing starts at 0,1,2...
-    
+
+
+
+    //static array : the size of the array is predefined while creating the array thus have a fix size 
+    uint256[3] public listofFavoriteNumber2 = [1,2,3];   //this is an array with the data uint ,indexing starts at 0,1,2...
+
 
 
     //we can create our own datatypes that groups multiple variables in solidity using the "struct"
-    
-
     struct person{
         uint256 favoriteNumber;
         string name;
@@ -20,10 +23,19 @@ contract simpleStorage{
     // person public pat = person({favoriteNumber:7, name : "pat"});
     // person public alex = person({favoriteNumber:8, name:"alex"});
 
+
     //instead of creating person one by one we can do that in an array[]
-
     person[] public listofpeople;
+    person[] public listofpeople2;
 
+
+    // pushing the data of the person in the listofpeople array
+    function addperson1(string memory _name,uint256 _favoriteNumber) public{
+        person memory Newperson = person(_favoriteNumber,_name);
+        listofpeople2.push(Newperson);
+    }
+
+    //Alternatively it can be done as below
     function addperson(string memory _name, uint256 _favoriteNumber)public {
         listofpeople.push(person(_favoriteNumber,_name));
     }
