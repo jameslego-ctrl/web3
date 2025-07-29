fbcd01eb7149f5ca2cf3f7c73a998edc8e6b7394
James <jameslego222@gmail.com>
2025-07-29 16:42:02 +0530

    added new file

--- practical/4FundMe.sol
+++ practical/4FundMe.sol
@@ -2,26 +2,66 @@
 
 pragma solidity ^0.8.18;
 
-import {priceConverter} from "./5priceConverter.sol";
+import {priceConverter} from "./priceconverter.sol";
+
+error notOwner();
 
 contract FundMe {
     using priceConverter for uint256;
-
+    uint256 public constant MINIMUM_USD = 5e18;
+    
     address[] public funders;
-    uint256 public minimumUsd = 5e18;
-    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address public immutable i_owner;
 
-    function fund() public payable{
-        require(msg.value.getConversionRate() >= minimumUsd,"didn't send enough ETH" );
+    constructor () {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "did'nt send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        // require(msg.sender == owner,"must be owner!");
+        for(uint256 funderIndex=0;funderIndex < funders.length;funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+    // reset the array
+        funders = new address[](0);
+    
+    // // actually withdraw the funds from the contract
+    //     // 3 ways to withdraw token or funds from a native blockchain
+    //     // transfer, send, call
+    //     // transfer
+    //     payable(msg.sender).transfer(address(this).balance);
+
+    //     // send 
+    //     bool sendSuccess = payable(msg.sender).send(address(this).balance);
+    //     require(sendSuccess,"send failed");
+
+        // call
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess,"call failed");
 
     }
 
-    //function withdraw() public {}
+    modifier onlyOwner {
+        // require(msg.sender == i_owner,"must be owner!");
+        if(msg.sender != i_owner) {revert notOwner();}
+        _;
+    }
 
+    receive() external payable {
+        fund();
+    }
 
+    fallback() external payable { 
+        fund();
+    }
+   
 }
-
-
-// 3796.440966000000000000
--- practical/5priceConverter.sol
+++ practical/5priceConverter.sol
@@ -4,21 +4,24 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-
 library priceConverter{
-     function getprice() internal view returns(uint256){
+    function getPrice () internal view returns(uint256) {
+        // Address
+        // ABI
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
         return uint256(price * 1e10);
     }
-
+    
     function getConversionRate(uint256 ethAmount) internal view returns(uint256){
-        uint256 ethPrice = getprice();
-        uint256 ethAmountInUsd = ethPrice * ethAmount;
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
-    }
 
-    function getversion() internal view returns(uint256){
+    }
+    function getVersion() internal view returns(uint256) {
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+
     }
 }
